4da701901279ba05b5e97321243a2e23dc7b828b
Tumo Labs <tumolabs@TUMOs-iMac.local>
2024-07-23 00:16:15 +0400

    deploying

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM python:3.9-alpine3.13
+
+ENV PYTHONUNBUFFERED 1
+
+COPY ./pong ./pong
+
+WORKDIR /pong
+EXPOSE 8000
+
+RUN apk add --update --no-cache
+
+RUN python -m venv /py && \
+    /py/bin/pip install --upgrade pip && \
+    /py/bin/pip install -r requirements.txt
+
+ENV PATH="/scripts:/py/bin:$PATH"
+
+VOLUME /pong
--- app.py
+++ app.py
@@ -1,35 +1,13 @@
-from flask import Flask, render_template
-from flask_admin import Admin
-from flask_admin.contrib.sqla import ModelView
-from flask_sqlalchemy import SQLAlchemy
-import api  
+from flask import Flask,render_template
 
-app = api.app  
+# Create a Flask application
+app = Flask(__name__)
 
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-
-
-class TaskModel(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    task = db.Column(db.String(200), nullable=False)
-
-
-admin = Admin(app, name='Admin Panel', template_mode='bootstrap3')
-admin.add_view(ModelView(TaskModel, db.session))
-
-
-print(f"Flask app name: {__name__}")
-print(f"App root path: {app.root_path}")
-
-@app.route('/home')
-def home():
+# Define a route for the root URL ("/")
+@app.route('/')
+def hello_world():
     return render_template('index.html')
 
+# Run the application
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.debug = True  
-    app.run()
+    app.run(debug=True)
