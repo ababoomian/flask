6f9289be03326fb589d52378dbe11b90ca9e6bc4
ababoomian <baboomian53@gmail.com>
2024-08-13 17:38:29 +0400

    ci/cd

--- get_host_ip.sh
+++ get_host_ip.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-HOST_IP=$(hostname -I | awk '{print $1}')
+# HOST_IP=$(hostname -I | awk '{print $1}')
+HOST_IP=http://35.170.192.165
 envsubst '${HOST_IP}' < /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/nginx.conf
 nginx -g 'daemon off;'
